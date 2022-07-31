e025e99734575807e220b853d02864358ee2a6e0
NeoAIChan <neoaichan@gmail.com>
2022-07-31 23:44:25 +0300

    Temporary Update.

--- script.js
+++
@@ -1,8 +0,0 @@
-function menuFunction() {
-  var menu = document.getElementById("myTopNav");
-  if (menu.className === "topNav") {
-    menu.className += " responsive";
-  } else {
-    menu.className = "topNav";
-  }
-}
