3c93db7f0123163b18401368dd66a05fac73f976
neoaichan@gmail.com <neoaichan@gmail.com>
2022-08-04 15:53:02 +0300

    Something new.

---
+++ script.js
@@ -0,0 +1,24 @@
+// all titles
+var titleArray = ["I am Neo! 👋🏻", "And, this is my website ❤️", "@Neodevil 🙌🏻"];
+
+// title's length 
+var titleArrayLength = titleArray.length;
+var i = 0;
+
+// interval function
+setInterval(titleUpdate, 3000);
+
+// title updating function
+function titleUpdate() {
+  
+  // looping the i variable
+  if (i == 3) {
+    i = 0;
+  }
+  
+  // updating title and increasing i variable 
+  document.title = titleArray[i];
+  i++;
+}
+
+//////////////////////////////////////
