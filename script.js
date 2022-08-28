e92adca8a5137b1c8a1bec1809151f4214c4983a
NeoAIChan <neoaichan@gmail.com>
2022-08-29 00:26:00 +0300

    Main Update

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 // all titles
-var titleArray = ["I am Neo! 👋🏻", "And, this is my website ❤️", "@Neodevil 🙌🏻"];
+var titleArray = [ "I am Neo! 👋🏻", "And, this is my website ❤️", "@Neodevil 🙌🏻" ];
 
-// title's length 
+// title's length
 var titleArrayLength = titleArray.length;
 var i = 0;
 
 // interval function
-setInterval(titleUpdate, 3000);
+setInterval( titleUpdate, 3000 );
 
 // title updating function
-function titleUpdate() {
+function titleUpdate () {
 
-  // looping the i variable
-  if (i == 3) {
-    i = 0;
-  }
+	// looping the i variable
+	if ( i == 3 ) {
+		i = 0;
+	}
 
-  // updating title and increasing i variable 
-  document.title = titleArray[i];
-  i++;
+	// updating title and increasing i variable
+	document.title = titleArray[ i ];
+	i++;
 }
-
-//////////////////////////////////////
